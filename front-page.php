880358875dab2cab028cf3851dcf0e8743966a6b
pnkfluffy <jacksonfelty@gmail.com>
2020-10-16 08:44:08 -0600

    nav and home, but overview is fked

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php
+/*
+@package nitketheme
+ */
+get_header();?>
+
+<div id="wonderplugin-fullscreen-slideshow">
+<?php echo do_shortcode('[wonderplugin_slider id=1]'); ?>
+</div>
+
+<?php get_footer();?>
