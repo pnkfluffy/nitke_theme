f7bf043fc4c9cd9f07a976ea3a0944ef1e9902ff
pnkfluffy <jacksonfelty@gmail.com>
2020-10-24 23:01:53 -0600

    Working dropdown and single pages

--- page.php
+++ page.php
@@ -1,1 +1,19 @@
-//static pages (not blog posts/shopping)
+<?php
+/*
+@package nitketheme
+ */
+get_header();?>
+<div id="primary" class="content-area">
+  <main id="main" class="page-main" role="main">
+    <?php
+if (have_posts()):
+    while (have_posts()): the_post();
+        get_template_part('template-parts/content', 'page');
+    endwhile;
+endif
+?>
+  </main>
+</div>
+
+
+<?php get_footer();?>
