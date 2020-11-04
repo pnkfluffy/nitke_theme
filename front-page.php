e0d49f08a4b533c7bcf506c0bd135ec92415fe74
pnkfluffy <jacksonfelty@gmail.com>
2020-11-04 16:41:41 -0700

    last changes, home page, hover effects, tiles

--- front-page.php
+++ front-page.php
@@ -3,7 +3,9 @@
 @package nitketheme
  */
 get_header();?>
-
+<style>
+<?php include 'css/nitke-home.css';?>
+</style>
 <div id="wonderplugin-fullscreen-slideshow">
 <?php echo do_shortcode('[wonderplugin_slider id=1]'); ?>
 </div>
