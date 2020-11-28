27723b6aa3c0a019787f098906fe2fdbd3d90be9
jacksonfelty@gmail.com <jacksonfelty@gmail.com>
2020-11-27 19:04:35 -0700

    tweaks to home and to mobile

--- front-page.php
+++ front-page.php
@@ -3,11 +3,17 @@
 @package nitketheme
  */
 get_header();?>
-<style>
+<!-- <style>
 <?php include 'css/nitke-home.css';?>
-</style>
+</style> -->
+
+<div id="primary-landing" class="content-area">
+  <main id="main" class="landing-main" role="main">
 <div id="wonderplugin-fullscreen-slideshow">
 <?php echo do_shortcode('[wonderplugin_slider id=1]'); ?>
 </div>
+</main>
+</div>
+
 
 <?php get_footer();?>
